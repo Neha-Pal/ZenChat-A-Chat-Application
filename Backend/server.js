1f1b17b9405f872d031cb5b230819093bf09a464
Neha Pal <palneha1779@gmail.com>
2024-10-03 19:34:45 +0530

    Connected to Database

--- Backend/server.js
+++ Backend/server.js
@@ -1,17 +1,24 @@
 import express from "express";
 import dotenv from "dotenv";
+import mongoose from "mongoose";
 
+dotenv.config()
+
+mongoose.connect(process.env.MONGO_URI).then(()=>{
+    console.log("MongoDB connected")
+})
+.catch((err)=>{
+    console.log(err)
+})
 
 const app = express();
 
-
-
+const PORT = process.env.PORT || 6000
 
 app.get("/",(req, res)=>{
     res.send("Backend")
 })
 
-const PORT = process.env.PORT || 6000
 app.listen(PORT, ()=>{
     console.log(`Server is running on PORT ${PORT}`)
 })
