0acf11d9b33ddd23a4e12acc3d6340853f1925c0
Neha Pal <palneha1779@gmail.com>
2024-10-03 21:55:21 +0530

    auth route setup

--- Backend/server.js
+++ Backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
+import authRoutes from "./routes/auth.routes.js"
 
 dotenv.config()
 
@@ -18,6 +19,10 @@ const PORT = process.env.PORT || 6000
 app.get("/",(req, res)=>{
     res.send("Backend")
 })
+
+//import routes
+
+app.use("/api/auth", authRoutes)
 
 app.listen(PORT, ()=>{
     console.log(`Server is running on PORT ${PORT}`)
